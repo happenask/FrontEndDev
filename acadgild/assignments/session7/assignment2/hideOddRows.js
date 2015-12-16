9f968e3b8ca8b1173d35842a5fa8ba3b209e5005
Sham <sham.ibmgs@gmail.com>
2015-12-16 18:27:10 +0530

    new version

---
+++ acadgild/assignments/session7/assignment2/hideOddRows.js
@@ -0,0 +1,10 @@
+/* hideOddRows.js */
+document.querySelector('[name=HideRows]').onclick = hideRows;
+
+function hideRows(){
+	var elements = document.getElementsByClassName('hide');
+	Array.prototype.forEach.call(elements, function(element){
+	    element.style.display = "none";
+	});
+	return true;
+}
